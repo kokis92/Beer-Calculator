e06a16e5d6427ea9dae96e552d8cdd055ea73245
kokis92 <kokis92@icloud.com>
2022-05-10 19:37:38 -0700

    Added modelo data

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 from prettytable import PrettyTable
 
+beer_chart = PrettyTable()
+
+beer_chart.field_names = ["Beer Name", "Calories", "ABV", "6pack/12pack", "Price", "Score", "Store", "Beer Type"]
+beer_chart.add_row(["Modelo", "144", "4.4%", "12pack", "$15.99", "", "Smart&Final", "Mexican Lager"])
+
+print(beer_chart)
