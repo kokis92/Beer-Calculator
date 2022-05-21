abda818561705ef888116136fa5b454c498ac999
Edi Reyes <eddyrya@gmail.com>
2022-05-21 15:02:43 -0700

    Update beerDatabase.py

--- beerDatabase.py
+++ beerDatabase.py
@@ -8,6 +8,7 @@ conn = pymssql.connect(server='localhost',
     database='BeerProject')
 
 # Interacting with the database 
+"""
 cursor = conn.cursor(as_dict=True)
 cursor.execute('SELECT * FROM Beer WHERE BeerName=%s', 'modelo')
 for row in cursor:
@@ -18,7 +19,21 @@ for row in cursor:
     row['ABV'] ))
 
 conn.close()
+"""
+#prints All of the Vallues in the ther database 
+c = conn.cursor(as_dict=True)
+c.execute("SELECT * FROM Beer WHERE BeerName = 'Modelo'")
+print(c.fetchall())
+conn.commit()
 
-
-
-
+"""
+c = conn.cursor(as_dict=True)
+c.execute("SELECT b.BeerName, s.BeerPrice, s.PackSize, s.StoreName, MIN(s.BeerPrice) AS LowerPrice
+From Beer b 
+INNER JOIN BeerStore s ON b.BeerID =s.BeerID
+WHere BeerName = 'Pacifico Clara' and PackSize = 12
+group by b.BeerName, s.BeerPrice, s.PackSize, s.StoreName
+order by s.BeerPrice ASC")
+print(c.fetchall())
+conn.commit()
+"""
