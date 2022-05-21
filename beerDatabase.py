78def92b4dcd0b57e47d728bb96c1be4a2ee517d
Edi Reyes <eddyrya@gmail.com>
2022-05-20 20:53:38 -0700

    Update beerDatabase.py

--- beerDatabase.py
+++ beerDatabase.py
@@ -1,4 +1,22 @@
-def test():
-    print("This is a test")
-#this is a test 
-test()
+#Creating a Microsoft Sql connection with python 
+#Python is running the queries from a local host 
+import _scproxy
+import pymssql  
+conn = pymssql.connect(server='localhost', 
+    user='sa', 
+    password='Reyes2356', 
+    database='BeerProject')
+
+# Interacting with the database 
+cursor = conn.cursor(as_dict=True)
+cursor.execute('SELECT * FROM Beer WHERE BeerName=%s', 'modelo')
+for row in cursor:
+    print("BeerID=%d, BeerName=%s , BeerType=%s, Calories=%s, ABV=%s" % (row['BeerID'], 
+    row['BeerName'], 
+    row['BeerType'],
+    row['Calories'],
+    row['ABV'] ))
+
+conn.close()
+
+
